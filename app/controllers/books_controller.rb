29aa8e50a73f72aed27088eebd2c26b8c636f9fe
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-07-31 19:02:11 -0500

    Generated the new and create actions and new's view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,24 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Saved!'
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :author)
+  end
 end
